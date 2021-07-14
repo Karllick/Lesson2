51ae5842a16a22abf8bb783b4c008abc1526a527
Zhenia Bulatov <karllick2000@gmail.com>
2021-07-14 22:20:41 +0500

    w

--- 2.py
+++ 2.py
@@ -1,1 +1,12 @@
 my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+new_list = []
+for i in my_list:
+    if i.replace('-', '').replace('+', '').isdigit():
+        if i.isdigit():
+            new_list.append(f'"{int(i):02}"')
+        else:
+            new_list.append(f'"{i[0]}{int(i[1:]):02}"')
+    else:
+        new_list.append(i)
+print(new_list)
+print(' '.join(new_list))
